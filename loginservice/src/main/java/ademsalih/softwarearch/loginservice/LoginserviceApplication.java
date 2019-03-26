0e8d52a737e61431c3f6bc985b83dbfec113fbf2
Adem Salih <adem@Adems-MacBook-Pro.local>
2019-03-26 11:06:55 +0100

    First commit

---
+++ loginservice/src/main/java/ademsalih/softwarearch/loginservice/LoginserviceApplication.java
@@ -0,0 +1,13 @@
+package ademsalih.softwarearch.loginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginserviceApplication.class, args);
+    }
+
+}
---
+++ tweetservice/src/main/java/ademsalih/softwarearch/tweetservice/TweetserviceApplication.java
@@ -0,0 +1,13 @@
+package ademsalih.softwarearch.tweetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetserviceApplication.class, args);
+    }
+
+}
