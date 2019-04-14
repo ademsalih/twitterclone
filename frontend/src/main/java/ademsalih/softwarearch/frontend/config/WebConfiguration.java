fde74a5236e3a2ea35f9f2fc921da9d76de009a5
Adem Salih <ademsal.97@icloud.com>
2019-04-14 13:44:55 +0200

    Fixed images not showing after upload and static files

--- frontend/src/main/java/ademsalih/softwarearch/frontend/config/WebConfiguration.java
+++ frontend/src/main/java/ademsalih/softwarearch/frontend/config/WebConfiguration.java
@@ -1,15 +1,20 @@
 package ademsalih.softwarearch.frontend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
 
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("file://" + System.getProperty("user.dir") + "/src/main/upload/");
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/static/",
+            "file://" + System.getProperty("user.dir") + "/src/main/upload/"
+    };
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
